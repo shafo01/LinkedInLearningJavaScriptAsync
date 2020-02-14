1c9c519763f77df033931453b1fca939655956ca
Shafeeza Hussain <shafeezahussain@yahoo.com>
2020-02-13 20:36:12 -0500

    call the timeout function

---
+++ timeout.js
@@ -0,0 +1,11 @@
+
+/**
+ * Call JavaScript Method setTimeout
+ * setTimeout took two parameters:
+ * function - in this case we created an asynchronous function
+ * using arrow syntax
+ */
+
+setTimeout(()=>{
+    console.log("Hello Time out finished");
+}, 5000);
